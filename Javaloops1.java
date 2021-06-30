618a5fd0502e381686b67e508d463b5c5ad7b58a
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2021-07-01 00:23:07 +0530

    Update and rename java-loops-1.java to Javaloops1.java

--- Javaloops1.java
+++ Javaloops1.java
@@ -8,7 +8,7 @@ import java.util.regex.*;
 
 
 
-public class Solution {
+public class Javaloops1 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
