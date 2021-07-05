c963f5228bbcc0dd31e07558f7b80f1cfcc6dc52
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2021-07-05 22:19:50 +0530

    Create StrToken.java

---
+++ StrToken.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class StrToken {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s=s.trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+        }
+        else{
+            String[] strArr = s.split("[?_!'.,@ ]+");
+            System.out.println(strArr.length);
+        
+            for(String result : strArr)
+                System.out.println(result);
+        }
+        scan.close();
+    }
+}
+
